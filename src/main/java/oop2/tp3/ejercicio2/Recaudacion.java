a5995e5c3ad03f164f000efb54fbc798eaecd51f
BenjaminRo01 <benjaroselo01@gmail.com>
2024-04-09 21:35:49 -0300

    Update Recaudacion.java

--- src/main/java/oop2/tp3/ejercicio2/Recaudacion.java
+++ src/main/java/oop2/tp3/ejercicio2/Recaudacion.java
@@ -8,79 +8,41 @@ import java.util.Map;
 
 public class Recaudacion {
     private List<String[]> csvData;
+    private List<String> columns;
 
     public Recaudacion() {
         csvData = LecturaCSV.getCsvData();
+        columns = List.of("permalink", "company_name", "number_employees", "category", "city", "state", "funded_date", "raised_amount", "raised_currency", "round");
     }
 
     public List<Map<String, String>> where(Map<String, String> options)
             throws IOException {
         List<Map<String, String>> output = new ArrayList<Map<String, String>>();
-        this.filter(options);
+        for (Map.Entry<String, String> entry : options.entrySet()) {
+            this.filter(entry.getKey(), entry.getValue());
+        }
         this.extracted(output);
         return output;
     }
 
-    private void extracted(List<Map<String, String>> output) {
+    private void filter(String key, String value) {
+        List<String[]> results = new ArrayList<String[]>();
+        int index = columns.indexOf(key);
         for (String[] data : csvData) {
-            Map<String, String> mapped = new HashMap<String, String>();
-            mapped.put("permalink", data[0]);
-            mapped.put("company_name", data[1]);
-            mapped.put("number_employees", data[2]);
-            mapped.put("category", data[3]);
-            mapped.put("city", data[4]);
-            mapped.put("state", data[5]);
-            mapped.put("funded_date", data[6]);
-            mapped.put("raised_amount", data[7]);
-            mapped.put("raised_currency", data[8]);
-            mapped.put("round", data[9]);
-            output.add(mapped);
+            if (data[index].equals(value)) {
+                results.add(data);
+            }
         }
+        csvData = results;
     }
 
-    private void filter(Map<String, String> options) {
-        if (options.containsKey("company_name")) {
-            List<String[]> results = new ArrayList<String[]>();
-
-            for (String[] data : csvData) {
-                if (data[1].equals(options.get("company_name"))) {
-                    results.add(data);
-                }
+    private void extracted(List<Map<String, String>> output) {
+        for (String[] data : csvData) {
+            Map<String, String> mapped = new HashMap<>();
+            for (int i = 0; i < columns.size(); i++) {
+                mapped.put(columns.get(i), data[i]);
             }
-            csvData = results;
-        }
-
-        if (options.containsKey("city")) {
-            List<String[]> results = new ArrayList<String[]>();
-
-            for (String[] data : csvData) {
-                if (data[4].equals(options.get("city"))) {
-                    results.add(data);
-                }
-            }
-            csvData = results;
-        }
-
-        if (options.containsKey("state")) {
-            List<String[]> results = new ArrayList<String[]>();
-
-            for (String[] data : csvData) {
-                if (data[5].equals(options.get("state"))) {
-                    results.add(data);
-                }
-            }
-            csvData = results;
-        }
-
-        if (options.containsKey("round")) {
-            List<String[]> results = new ArrayList<String[]>();
-
-            for (String[] data : csvData) {
-                if (data[9].equals(options.get("round"))) {
-                    results.add(data);
-                }
-            }
-            csvData = results;
+            output.add(mapped);
         }
     }
 }
