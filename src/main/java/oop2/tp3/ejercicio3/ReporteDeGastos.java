8aa9b93e7203086e7a210d2ad6c8a68e0500aaf4
BenjaminRo01 <benjaroselo01@gmail.com>
2024-04-18 21:08:45 -0300

    Se elimino FormatoReporte
    No es necesario.

--- src/main/java/oop2/tp3/ejercicio3/ReporteDeGastos.java
+++ src/main/java/oop2/tp3/ejercicio3/ReporteDeGastos.java
@@ -1,21 +1,23 @@
 package oop2.tp3.ejercicio3;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class ReporteDeGastos {
     public String imprimir(List<Gasto> gastos) {
         int total = 0;
         int gastosDeComida = 0;
-        StringBuilder reporte = new StringBuilder(FormatoReporte.formatoCabecera());
+        StringBuilder reporte = new StringBuilder("Expenses " + LocalDate.now() + "\n");
         for (Gasto gasto : gastos) {
             gastosDeComida += gasto.montoComidas();
-            reporte.append(FormatoReporte.formatoLineaGasto(gasto));
+            String strExcesoGastoComida = gasto.excesoDeGastoComida() ? "X" : " ";
+            reporte.append(gasto.obtenerNombre()).append("\t")
+                    .append(gasto.obtenerMonto()).append("\t")
+                    .append(strExcesoGastoComida).append("\n");
             total += gasto.monto();
         }
-
-        reporte.append(FormatoReporte.formatoGastosDeComida(gastosDeComida));
-        reporte.append(FormatoReporte.formatoTotal(total));
-
+        reporte.append("Gastos de comida: ").append(gastosDeComida).append("\n");
+        reporte.append("Total de gastos: ").append(total);
         return reporte.toString();
     }
 }
